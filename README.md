f1ed4b5a07e5af6bf51345be0f7599fd061bb325
Hamad Tariq <90149450+hamadbintariq4u@users.noreply.github.com>
2022-11-13 21:35:54 +0500

    add login signup pages

--- README.md
+++ README.md
@@ -1,16 +1,5 @@
-# book_bank
+# Login UI made with flutter.
 
-A new Flutter project.
+## Screenshot
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+![Screenshot1](https://github.com/hawier-dev/flutter-login-ui/blob/main/screenshots/login1.png) ![Screenshot2](https://github.com/hawier-dev/flutter-login-ui/blob/main/screenshots/login2.png)
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,38 @@
-// ignore_for_file: avoid_unnecessary_containers
-
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+import 'login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+ColorScheme defaultColorScheme = const ColorScheme(
+  primary: Color(0xffBB86FC),
+  secondary: Color(0xff03DAC6),
+  surface: Color(0xff181818),
+  background: Color(0xff121212),
+  error: Color(0xffCF6679),
+  onPrimary: Color(0xff000000),
+  onSecondary: Color(0xff000000),
+  onSurface: Color(0xffffffff),
+  onBackground: Color(0xffffffff),
+  onError: Color(0xff000000),
+  brightness: Brightness.dark,
+);
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: defaultColorScheme,
+        primarySwatch: Colors.blue,
       ),
+      home: const LoginPage(title: 'Login UI'),
     );
   }
 }
