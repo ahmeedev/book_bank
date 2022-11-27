c06dc51af29ffbc6ab59f32036c91b73d4285986
Hamad Tariq <90149450+hamadbintariq4u@users.noreply.github.com>
2022-11-27 10:19:36 +0500

    add fierbase and remove errors

--- lib/app/modules/introduction/views/introduction_view.dart
+++ lib/app/modules/introduction/views/introduction_view.dart
@@ -1,9 +1,8 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import '../../../theme/app_constants.dart';
 import '../controllers/introduction_controller.dart';
 
 class IntroductionView extends GetView<IntroductionController> {
@@ -11,19 +10,19 @@ class IntroductionView extends GetView<IntroductionController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff8599FF),
+      backgroundColor: const Color(0xff8599FF),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          SizedBox(
+          const SizedBox(
             height: 80,
           ),
           Center(
             child: Image.network(
                 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPKZig7TVNF_CbKFPdYJ4t8jCGVw6nncU82M_JFzAArbKaa2R1YkBEzmy_w2qhTnGEzAM&usqp=CAU"),
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 20.0, top: 90),
+          const Padding(
+            padding: EdgeInsets.only(left: 20.0, top: 90),
             child: Text(
               "Read Your\nFaviroute book\nfrom here",
               style: TextStyle(
@@ -33,8 +32,8 @@ class IntroductionView extends GetView<IntroductionController> {
               ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
+          const Padding(
+            padding: EdgeInsets.all(20.0),
             child: Text(
               "Lorem Ipsum is simply dummy text\nof the printing and typesetting industry.",
               style: TextStyle(
@@ -43,21 +42,21 @@ class IntroductionView extends GetView<IntroductionController> {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.only(left: 50.0, top: 40),
+            padding: const EdgeInsets.only(left: kPadding),
             child: ClipRRect(
               borderRadius: BorderRadius.circular(20),
               child: Container(
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(40),
                 ),
-                width: 400,
+                width: 350,
                 height: 80,
                 child: ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xff151E47),
+                    backgroundColor: const Color(0xff151E47),
                   ),
                   onPressed: () {},
-                  child: Text(
+                  child: const Text(
                     "Get Started",
                     style: TextStyle(color: Colors.white),
                   ),
---
+++ lib/app/theme/app_constants.dart
@@ -0,0 +1,2 @@
+const kPadding=16.0;
+const kMargin=16.0;
