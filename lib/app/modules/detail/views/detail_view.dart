5f5ce73abb4dcbc1ad173f752fa064abb2c0309f
Ahmad Tariq <ahmadbintariq4u@gmail.com>
2022-12-03 13:23:13 +0500

    details view updated!

--- lib/app/modules/detail/views/detail_view.dart
+++ lib/app/modules/detail/views/detail_view.dart
@@ -1,10 +1,12 @@
-import 'package:book_bank/app/modules/cart/controllers/cart_controller.dart';
-import 'package:book_bank/app/utilities/get_methods.dart';
+import 'package:book_bank/app/routes/app_pages.dart';
+import 'package:book_bank/app/theme/colors.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
 import '../../../theme/app_constants.dart';
+import '../../../utilities/get_methods.dart';
+import '../../cart/controllers/cart_controller.dart';
 import '../controllers/detail_controller.dart';
 
 class DetailView extends GetView<DetailController> {
@@ -13,86 +15,98 @@ class DetailView extends GetView<DetailController> {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return Scaffold(
-        appBar: AppBar(title: const Text("Book Details")),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              // margin: const EdgeInsets.all(kMargin),
-              height: 300,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                // borderRadius: BorderRadius.circular(20),
-                image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: NetworkImage(controller.book.image)),
-              ),
+      appBar: AppBar(title: const Text("Book Details")),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            // margin: const EdgeInsets.all(kMargin),
+            height: 300,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              // borderRadius: BorderRadius.circular(20),
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(controller.book.image)),
             ),
-            Expanded(
-              child: SingleChildScrollView(
-                physics: const BouncingScrollPhysics(),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    kHeight,
-                    Text(
-                      controller.book.name,
-                      style: theme.textTheme.titleLarge!
-                          .copyWith(color: Colors.black),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          controller.book.authur,
-                          style: theme.textTheme.labelMedium!
-                              .copyWith(color: Colors.black),
-                        ),
-                        Text(
-                          "Rs. ${controller.book.price}",
-                          style: theme.textTheme.titleLarge!
-                              .copyWith(color: theme.colorScheme.primary),
-                        ),
-                      ],
-                    ),
-                    kHeight,
-                    Text(
-                      "Description",
-                      style: theme.textTheme.titleLarge!.copyWith(
-                          color: Colors.black, fontWeight: FontWeight.w900),
-                    ),
-                    kHeight,
-                    Text(
-                      controller.book.description,
-                      style: theme.textTheme.labelMedium!
-                          .copyWith(color: Colors.black),
-                    ),
-                  ],
-                ).paddingSymmetric(horizontal: kPadding),
-              ),
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  kHeight,
+                  Text(
+                    controller.book.name,
+                    style: theme.textTheme.titleLarge!
+                        .copyWith(color: Colors.black),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        controller.book.authur,
+                        style: theme.textTheme.labelMedium!
+                            .copyWith(color: Colors.black),
+                      ),
+                      Text(
+                        "Rs. ${controller.book.price}",
+                        style: theme.textTheme.titleLarge!
+                            .copyWith(color: theme.colorScheme.primary),
+                      ),
+                    ],
+                  ),
+                  kHeight,
+                  Text(
+                    "Description",
+                    style: theme.textTheme.titleLarge!.copyWith(
+                        color: Colors.black, fontWeight: FontWeight.w900),
+                  ),
+                  kHeight,
+                  Text(
+                    controller.book.description,
+                    style: theme.textTheme.labelMedium!
+                        .copyWith(color: Colors.black),
+                  ),
+                ],
+              ).paddingSymmetric(horizontal: kPadding),
             ),
-            Row(
-              children: [
-                Expanded(
-                    child: ElevatedButton(
-                        onPressed: () {
-                          Get.find<CartController>()
-                              .cartItems
-                              .add(controller.book);
+          ),
+          Row(
+            children: [
+              Expanded(
+                  // flex: 2,
+                  child: ElevatedButton(
+                      onPressed: () {
+                        Get.toNamed(Routes.PDF);
+                      },
+                      child: const Text("Read").paddingAll(kPadding))),
+              kWidth,
+              Expanded(
+                  flex: 2,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: theme.colorScheme.secondary),
+                      onPressed: () {},
+                      child: const Text("Buy Now").paddingAll(kPadding))),
+            ],
+          ).paddingSymmetric(vertical: kPadding / 2, horizontal: kPadding),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Get.find<CartController>().cartItems.add(controller.book);
 
-                          showSnackBar(
-                              title: "Cart",
-                              description: "Items added to cart successfully");
-                        },
-                        child: const Text("Add to cart").paddingAll(kPadding))),
-                kWidth,
-                Expanded(
-                    child: ElevatedButton(
-                        onPressed: () {},
-                        child: const Text("Buy Now").paddingAll(kPadding))),
-              ],
-            ).paddingSymmetric(horizontal: kPadding, vertical: kPadding / 2),
-          ],
-        ));
+          showSnackBar(
+              title: "Cart", description: "Items added to cart successfully");
+        },
+        child: Icon(
+          Icons.shopping_cart,
+          color: defaultColorScheme.onPrimary,
+        ),
+      ),
+    );
   }
 }
