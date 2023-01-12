6ca522aee14f584abdd8fdfa4d052aa4c6793ebe
Hamad Tariq <90149450+hamadbintariq4u@users.noreply.github.com>
2023-01-12 21:22:50 +0500

    hlo\

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ import 'package:firebase_core/firebase_core.dart';
 
 import 'app/theme/light_theme.dart';
 import 'firebase_options.dart';
-
+// hello
 var logger = Logger();
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
